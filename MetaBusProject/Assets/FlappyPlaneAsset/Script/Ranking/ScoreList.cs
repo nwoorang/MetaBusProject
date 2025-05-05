40f03c62c86d96ccb3076ebc46dbb4d3ebf46eac
nwoorang <89909664+nwoorang@users.noreply.github.com>
2025-05-06 05:52:25 +0900

    Add:Tmpmesh,UI
    UI추가 및 TMP 리소스 추가

---
+++ MetaBusProject/Assets/FlappyPlaneAsset/Script/Ranking/ScoreList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ScoreList
+{
+    public List<ScoreRecord> scores = new List<ScoreRecord>();
+}
